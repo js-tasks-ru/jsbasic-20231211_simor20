db49dcd539cb93f9ab3e9efccf0cadb14830ce16
Мария Живкович <rogerbarrett.046@gmail.com>
2023-12-16 13:07:18 +0300

    решение 1 задачи из 1 модуля

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
 function factorial(n) {
-  // ваш код...
+  let result;
+
+  if (n > 1) {
+    result = n * (n - 1);
+    for (let i = n - 2; i > 0; i--) {
+      result = result * i;
+    }
+  } else {
+    result = 1;
+  }
+
+  return result;
 }
