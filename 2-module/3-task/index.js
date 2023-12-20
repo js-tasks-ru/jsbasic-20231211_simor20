428639783c335020f96a7631ccd3a75c7fb6f7c9
Мария Живкович <rogerbarrett.046@gmail.com>
2023-12-21 02:40:42 +0300

    решение 1, 2, 3 задачи из 2 модуля

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,18 @@
 let calculator = {
-  // ваш код
+  read(a, b) {
+    Object.assign(this, { a, b });
+  },
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  },
 };
+
+calculator.read(3, 5);
+console.log(calculator.sum());
+console.log(calculator.mul());
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
