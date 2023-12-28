c0927509bd16b55cf2a1c2435ed941e73edcf726
Мария Живкович <rogerbarrett.046@gmail.com>
2023-12-28 17:24:21 +0300

    решение 4, 5 задачи 3 модуля

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,9 @@
 function getMinMax(str) {
-  // ваш код...
+  const arrFromStr = str.split(" ").map((element) => Number(element));
+  const onlyNumbers = arrFromStr.filter((element) => !isNaN(element));
+
+  return {
+    min: Math.min(...onlyNumbers),
+    max: Math.max(...onlyNumbers),
+  };
 }
