94a4f1bd6ca6e1a20575754f8e50f4a80814d4f2
Мария Живкович <rogerbarrett.046@gmail.com>
2024-01-04 14:09:03 +0300

    решение задач 4 модуля

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,27 @@
 function highlight(table) {
-  // ваш код...
+  let tBodyRows = table.querySelectorAll("tbody > tr");
+
+  for (let row of tBodyRows) {
+    let availability = row.cells[3].dataset.available;
+
+    if (availability === "true") {
+      row.classList.add("available");
+    } else if (availability === "false") {
+      row.classList.add("unavailable");
+    } else if (availability === undefined) {
+      row.hidden = true;
+    }
+
+    let gender = row.cells[2].innerHTML;
+
+    if (gender === "m") {
+      row.classList.add("male");
+    } else {
+      row.classList.add("female");
+    }
+
+    if (row.cells[1].innerHTML < 18) {
+      row.style.textDecoration = "line-through";
+    }
+  }
 }
